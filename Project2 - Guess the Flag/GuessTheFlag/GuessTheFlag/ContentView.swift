23906521fd483ac679b785690429c7c2340dcc9c
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2020-02-03 15:10:08 +0200

    Moved Guess the Flag empty project to Project folder

---
+++ Project2 - Guess the Flag/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Rinalds Domanovs on 01/02/2020.
+//  Copyright © 2020 Rinalds Domanovs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("SwiftUI")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
