a30f7f557b34bcd32a4360376c3acec50354a654
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2020-02-04 13:43:12 +0200

    feat: Added score state and incrementing it when user answered correctly and decrementing when incorrectly

--- Project2 - Guess the Flag/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Project2 - Guess the Flag/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct ContentView: View {
     @State private var showingScore = false
     @State private var scoreTitle = ""
+    @State private var score = 0
 
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
@@ -44,7 +45,7 @@ struct ContentView: View {
             }
         }
         .alert(isPresented: $showingScore) {
-            Alert(title: Text(scoreTitle), message: Text("Your score is ???"), dismissButton: .default(Text("Continue")) {
+            Alert(title: Text(scoreTitle), message: Text("Your score is \(score)"), dismissButton: .default(Text("Continue")) {
                 self.askQuestion()
             })
         }
@@ -53,8 +54,10 @@ struct ContentView: View {
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
             scoreTitle = "Correct"
+            score += 10
         } else {
             scoreTitle = "Wrong"
+            score -= 5
         }
 
         showingScore = true
