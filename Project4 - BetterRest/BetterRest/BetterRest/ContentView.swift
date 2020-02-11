e14b6ccdbb39f29738a61db0a2ac04c5f5016157
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2020-02-11 06:33:10 +0200

    refactoring VStack into Form and making date picker to be a WheelDatePicker

--- Project4 - BetterRest/BetterRest/BetterRest/ContentView.swift
+++ Project4 - BetterRest/BetterRest/BetterRest/ContentView.swift
@@ -26,12 +26,13 @@ struct ContentView: View {
 
     var body: some View {
         NavigationView {
-            VStack {
+            Form {
                 Text("When do you want to wake up?")
                     .font(.headline)
 
                 DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
                     .labelsHidden()
+                    .datePickerStyle(WheelDatePickerStyle())
 
                 Text("Desired amount of sleep")
                     .font(.headline)
