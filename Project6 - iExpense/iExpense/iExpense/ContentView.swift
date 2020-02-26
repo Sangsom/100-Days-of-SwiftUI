68e9ee2c3c6e5c4a4c2032cb8ebf3e7fbf76d088
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2020-02-26 06:22:06 +0200

    Moved iExpense starter project to project folder

---
+++ Project6 - iExpense/iExpense/iExpense/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Rinalds Domanovs on 25/02/2020.
+//  Copyright © 2020 Rinalds Domanovs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Starter")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project6 - iExpense/iExpense/iExpense/Views/ExpenseItem.swift
@@ -0,0 +1,15 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by Rinalds Domanovs on 26/02/2020.
+//  Copyright © 2020 Rinalds Domanovs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Int
+}
