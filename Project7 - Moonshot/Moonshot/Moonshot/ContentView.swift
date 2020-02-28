ad7211b8a41b030e54c2c1d62933c3448774e920
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2020-02-28 13:58:25 +0200

    Moved Moonshot initial project folder

---
+++ Project7 - Moonshot/Moonshot/Moonshot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Rinalds Domanovs on 27/02/2020.
+//  Copyright © 2020 Rinalds Domanovs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Helo")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
