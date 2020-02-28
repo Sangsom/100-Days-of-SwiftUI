9512974002f9c0568450704fbc93006cddb17e6a
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2020-02-28 14:06:54 +0200

    Created Astronaut model

---
+++ Project7 - Moonshot/Moonshot/Moonshot/Models/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Rinalds Domanovs on 28/02/2020.
+//  Copyright © 2020 Rinalds Domanovs. All rights reserved.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
