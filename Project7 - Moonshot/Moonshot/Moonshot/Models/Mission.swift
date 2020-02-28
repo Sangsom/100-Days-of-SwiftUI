4e1166e715a2c4c05191fd4a053e36c4f8aef405
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2020-02-28 14:26:07 +0200

    Created Mission model

--- Project7 - Moonshot/Moonshot/Moonshot/Models/Mission.swift
+++ Project7 - Moonshot/Moonshot/Moonshot/Models/Mission.swift
@@ -8,7 +8,14 @@
 
 import Foundation
 
-struct CrewRole: Codable {
-    let name: String
-    let role: String
+struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+
+    let id: Int
+    let launchDate: String?
+    let crew: [CrewRole]
+    let description: String
 }
