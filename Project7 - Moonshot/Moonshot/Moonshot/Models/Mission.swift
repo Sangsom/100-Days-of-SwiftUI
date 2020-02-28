8182fb8ea63fd7cb3743dccd91b6eb2e8df9aa50
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2020-02-28 14:23:12 +0200

    feat: Created CrewRole model

---
+++ Project7 - Moonshot/Moonshot/Moonshot/Models/Mission.swift
@@ -0,0 +1,14 @@
+//
+//  Mission.swift
+//  Moonshot
+//
+//  Created by Rinalds Domanovs on 28/02/2020.
+//  Copyright © 2020 Rinalds Domanovs. All rights reserved.
+//
+
+import Foundation
+
+struct CrewRole: Codable {
+    let name: String
+    let role: String
+}
