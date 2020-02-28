d1db4b7c47ef7e04c97a7d9a0ebbabf20be3b44f
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2020-02-28 15:17:52 +0200

    Created MissionView empty view

---
+++ Project7 - Moonshot/Moonshot/Moonshot/Views/MissionView.swift
@@ -0,0 +1,21 @@
+//
+//  MissionView.swift
+//  Moonshot
+//
+//  Created by Rinalds Domanovs on 28/02/2020.
+//  Copyright © 2020 Rinalds Domanovs. All rights reserved.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MissionView_Previews: PreviewProvider {
+    static var previews: some View {
+        MissionView()
+    }
+}
