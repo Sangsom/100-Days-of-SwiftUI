b33ec288376d5fb2e4f0f6f6758db0ef6eccaec7
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2020-02-29 06:46:15 +0200

    Showing all missions that astronaut has flew to

--- Project7 - Moonshot/Moonshot/Moonshot/Views/AstronautView.swift
+++ Project7 - Moonshot/Moonshot/Moonshot/Views/AstronautView.swift
@@ -10,9 +10,26 @@ import SwiftUI
 
 struct AstronautView: View {
     let astronaut: Astronaut
+    let missions: [Mission]
+
+    init(astronaut: Astronaut) {
+        self.astronaut = astronaut
+
+        let loadMissions: [Mission] = Bundle.main.decode("missions.json")
+        var matchMissions = [Mission]()
+
+        for mission in loadMissions {
+            if mission.crew.first(where: { $0.name == astronaut.id }) != nil {
+                matchMissions.append(mission)
+            }
+        }
+
+        self.missions = matchMissions
+    }
 
     var body: some View {
-        GeometryReader { geometry in
+        print(astronaut)
+        return GeometryReader { geometry in
             ScrollView(.vertical) {
                 VStack {
                     Image(self.astronaut.id)
@@ -23,6 +40,21 @@ struct AstronautView: View {
                     Text(self.astronaut.description)
                         .padding()
                         .layoutPriority(1)
+
+                    Group {
+                        Text("Missions")
+                            .font(.headline)
+
+                        ForEach(self.missions, id: \.id) { mission in
+                            VStack(alignment: .leading) {
+                                Text(mission.displayName)
+                                Text(mission.formattedLaunchDate)
+                            }
+                        }
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal)
+
                 }
             }
         }
@@ -34,6 +66,6 @@ struct AstronautView_Previews: PreviewProvider {
     static let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
 
     static var previews: some View {
-        AstronautView(astronaut: astronauts[0])
+        AstronautView(astronaut: astronauts[3])
     }
 }
