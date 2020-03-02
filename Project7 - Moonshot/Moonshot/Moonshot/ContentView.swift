a579ceabc4fd69919d820cf11e84692583c7972e
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2020-03-02 11:47:42 +0200

    Outputting astronaut names in when switched to show names

--- Project7 - Moonshot/Moonshot/Moonshot/ContentView.swift
+++ Project7 - Moonshot/Moonshot/Moonshot/ContentView.swift
@@ -12,6 +12,8 @@ struct ContentView: View {
     let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
 
+    @State private var showingNames = false
+
     var body: some View {
         NavigationView {
             List(missions) { mission in
@@ -24,11 +26,46 @@ struct ContentView: View {
                     VStack(alignment: .leading) {
                         Text(mission.displayName)
                             .font(.headline)
-                        Text(mission.formattedLaunchDate)
+                        if self.showingNames {
+                            VStack {
+                                MissionNames(mission: mission)
+                            }
+                        } else {
+                            Text(mission.formattedLaunchDate)
+                        }
                     }
                 }
             }
             .navigationBarTitle("Moonshot")
+            .navigationBarItems(trailing: Button(showingNames ? "Show Dates" : "Show Names") {
+                self.showingNames.toggle()
+            })
+        }
+    }
+}
+
+struct MissionNames: View {
+    var mission: Mission
+    var astronauts: [Astronaut]
+
+    init(mission: Mission) {
+        self.mission = mission
+
+        let loadAstronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
+        var matchAstronauts = [Astronaut]()
+
+        for astronaut in loadAstronauts {
+            if mission.crew.first(where: { $0.name == astronaut.id }) != nil {
+                matchAstronauts.append(astronaut)
+            }
+        }
+
+        self.astronauts = matchAstronauts
+    }
+
+    var body: some View {
+        ForEach(self.astronauts, id: \.id) {
+            Text("\($0.name)")
         }
     }
 }
