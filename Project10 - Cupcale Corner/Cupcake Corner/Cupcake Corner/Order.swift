53e59ba0428656797f769fce8ba5a1360583fcca
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2020-03-13 09:09:45 +0200

    Created Order class

---
+++ Project10 - Cupcale Corner/Cupcake Corner/Cupcake Corner/Order.swift
@@ -0,0 +1,20 @@
+//
+//  Order.swift
+//  Cupcake Corner
+//
+//  Created by Rinalds Domanovs on 13/03/2020.
+//  Copyright © 2020 Rinalds Domanovs. All rights reserved.
+//
+
+import SwiftUI
+
+class Order: ObservableObject {
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+
+    @Published var type = 0
+    @Published var quantity = 3
+
+    @Published var specialRequestEnabled = false
+    @Published var extraFrosting = false
+    @Published var addSprinkles = false
+}
