25415e656c161d2630b1f8f0301349009b92498f
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2020-03-13 09:18:48 +0200

    Created form for selecting cake type and amount

--- Project10 - Cupcale Corner/Cupcake Corner/Cupcake Corner/ContentView.swift
+++ Project10 - Cupcale Corner/Cupcake Corner/Cupcake Corner/ContentView.swift
@@ -12,7 +12,22 @@ struct ContentView: View {
     @ObservedObject var order = Order()
 
     var body: some View {
-        Text("Hello")
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $order.type) {
+                        ForEach(0..<Order.types.count) {
+                            Text(Order.types[$0])
+                        }
+                    }
+
+                    Stepper(value: $order.quantity, in: 3...20) {
+                        Text("Number of cakes: \(order.quantity)")
+                    }
+                }
+            }
+        .navigationBarTitle("Cupcake Corner")
+        }
     }
 }
 
