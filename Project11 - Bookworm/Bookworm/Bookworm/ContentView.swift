62704b8f8c185c1ae4f3e7e190662d46a90f256d
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2020-03-16 12:48:14 +0200

    Moved empty app project folder to project

---
+++ Project11 - Bookworm/Bookworm/Bookworm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bookworm
+//
+//  Created by Rinalds Domanovs on 16/03/2020.
+//  Copyright © 2020 Rinalds Domanovs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Text")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
