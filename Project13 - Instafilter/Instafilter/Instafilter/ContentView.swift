c7ad826ad416071372806acc457046cdc6ac37ca
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2020-04-01 13:39:09 +0300

    Created sample app UI

--- Project13 - Instafilter/Instafilter/Instafilter/ContentView.swift
+++ Project13 - Instafilter/Instafilter/Instafilter/ContentView.swift
@@ -9,8 +9,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var image: Image?
+    @State private var filterIntensity = 0.5
+
     var body: some View {
-        Text("Hello")
+        NavigationView {
+            ZStack {
+                Rectangle()
+                    .fill(Color.secondary)
+
+                // display the image
+            }
+            .onTapGesture {
+                // select an image
+            }
+
+            HStack {
+                Text("Intensity")
+                Slider(value: $filterIntensity)
+            }.padding(.vertical)
+
+            HStack {
+                Button("Change Filter") {
+                    // change filter
+                }
+
+                Spacer()
+
+                Button("Save") {
+                    // save the picture
+                }
+            }
+        }
+        .padding([.horizontal, .bottom])
+        .navigationBarTitle("Instafilter")
     }
 }
 
--- Project6 - iExpense/iExpense/iExpense/ContentView.swift
+++ Project6 - iExpense/iExpense/iExpense/ContentView.swift
@@ -37,6 +37,7 @@ struct ContentView: View {
                 }) {
                     Image(systemName: "plus")
                 }
+                .padding()
             )
         }
         .sheet(isPresented: $showingAddExpenses) {
