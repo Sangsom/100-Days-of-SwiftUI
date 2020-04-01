edbb478da48b0816fe9eab91348c0312ddb71614
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2020-04-01 09:56:46 +0300

    Moved empty Instafilter project folder

---
+++ Project13 - Instafilter/Instafilter/Instafilter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by Rinalds Domanovs on 31/03/2020.
+//  Copyright © 2020 Rinalds Domanovs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
