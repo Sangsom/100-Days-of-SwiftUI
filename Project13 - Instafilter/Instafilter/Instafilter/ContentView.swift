8492b524bd2b375e93454ee152fd6bc6db21d0b6
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2020-04-01 14:25:10 +0300

    Loading image when it's selected

--- Project13 - Instafilter/Instafilter/Instafilter/ContentView.swift
+++ Project13 - Instafilter/Instafilter/Instafilter/ContentView.swift
@@ -11,6 +11,8 @@ import SwiftUI
 struct ContentView: View {
     @State private var image: Image?
     @State private var filterIntensity = 0.5
+    @State private var showingImagePicker = false
+    @State private var inputImage: UIImage?
 
     var body: some View {
         NavigationView {
@@ -30,7 +32,7 @@ struct ContentView: View {
                 }
             }
             .onTapGesture {
-                // select an image
+                self.showingImagePicker = true
             }
 
             HStack {
@@ -52,6 +54,14 @@ struct ContentView: View {
         }
         .padding([.horizontal, .bottom])
         .navigationBarTitle("Instafilter")
+        .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
+            ImagePicker(image: self.$inputImage)
+        }
+    }
+
+    func loadImage() {
+        guard let inputImage = inputImage else { return }
+        image = Image(uiImage: inputImage)
     }
 }
 
