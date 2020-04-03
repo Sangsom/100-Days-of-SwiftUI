88c2f5c863aec8831cd088ef4e710689c9cbc853
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2020-04-03 13:59:49 +0300

    Created sample EditView

---
+++ Project14 - Bucket List/BucketList/BucketList/EditView.swift
@@ -0,0 +1,36 @@
+//
+//  EditView.swift
+//  BucketList
+//
+//  Created by Rinalds Domanovs on 03/04/2020.
+//  Copyright © 2020 Rinalds Domanovs. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct EditView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @ObservedObject var placemark: MKPointAnnotation
+
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Place name", text: $placemark.wrappedTitle)
+                    TextField("Description", text: $placemark.wrappedSubtitle)
+                }
+            }
+        .navigationBarTitle("Edit place")
+            .navigationBarItems(trailing: Button("Done") {
+                self.presentationMode.wrappedValue.dismiss()
+            })
+        }
+    }
+}
+
+struct EditView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditView(placemark: MKPointAnnotation.example)
+    }
+}
