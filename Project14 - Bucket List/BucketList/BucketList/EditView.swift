0064c2380988e98aa38d6d2f8180ed7f318b7c1c
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2020-04-03 14:58:44 +0300

    Showing page description if there are

--- Project14 - Bucket List/BucketList/BucketList/EditView.swift
+++ Project14 - Bucket List/BucketList/BucketList/EditView.swift
@@ -33,7 +33,7 @@ struct EditView: View {
                             Text(page.title)
                                 .font(.headline)
                             + Text(": ") +
-                            Text("Page description here")
+                                Text(page.description)
                                 .italic()
                         }
                     } else if loadingState == .loading {
