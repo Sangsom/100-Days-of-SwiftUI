86fc087e94afcc229aa67b0aede35f5a4353af5f
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2020-04-03 14:47:31 +0300

    Fetching nearby places from Wikipedia and showing in EditView

--- Project14 - Bucket List/BucketList/BucketList/EditView.swift
+++ Project14 - Bucket List/BucketList/BucketList/EditView.swift
@@ -48,6 +48,33 @@ struct EditView: View {
                 self.presentationMode.wrappedValue.dismiss()
             })
         }
+        .onAppear(perform: fetchNearbyPlaces)
+    }
+
+    func fetchNearbyPlaces() {
+        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(placemark.coordinate.latitude)%7C\(placemark.coordinate.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
+
+        guard let url = URL(string: urlString) else {
+            print("Bad URL: \(urlString)")
+            return
+        }
+
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data {
+                // we got some data back!
+                let decoder = JSONDecoder()
+
+                if let items = try? decoder.decode(Result.self, from: data) {
+                    // success - convert the array values to our pages array
+                    self.pages = Array(items.query.pages.values)
+                    self.loadingState = .loaded
+                    return
+                }
+            }
+
+            // if we're still here it means the request failed somehow
+            self.loadingState = .failed
+        }.resume()
     }
 }
 
