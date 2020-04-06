28b0ad3bdccc80e99f88939938014e142f88072d
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2020-04-06 10:16:17 +0300

    Added Challenge6 Project Meetup Memory empty project

---
+++ Challenge6 - Meetup Memory/MeetupMem/MeetupMem/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MeetupMem
+//
+//  Created by Rinalds Domanovs on 06/04/2020.
+//  Copyright © 2020 Rinalds Domanovs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
